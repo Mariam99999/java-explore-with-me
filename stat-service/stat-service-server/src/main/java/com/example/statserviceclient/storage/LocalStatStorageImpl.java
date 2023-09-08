fbfd640c4b3211d3136690036a858d82130c2d8d
Mariam <mariam_agadzhanyan@mail.ru>
2023-09-08 22:46:57 +0300

    fix storage

--- stat-service/stat-service-server/src/main/java/com/example/statserviceclient/storage/LocalStatStorageImpl.java
+++ stat-service/stat-service-server/src/main/java/com/example/statserviceclient/storage/LocalStatStorageImpl.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Component;
 import java.io.UnsupportedEncodingException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
@@ -28,12 +28,26 @@ public class LocalStatStorageImpl implements StatStorage {
 
     @Override
     public List<StatDtoGet> getStat(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
-        if (unique) return null;
         List<StatDtoGet> result = new ArrayList<>();
-        for (String uri : uris) {
-            long hits = storage.stream().filter(s -> s.getUri().equals(uri)&& !s.getTimestamp().isBefore(start)
-            && !s.getTimestamp().isAfter(end)).count();
-            result.add(new StatDtoGet(APP,uri,hits));
+        Set<String> ips = new HashSet<>();
+        if (uris != null && !uris.isEmpty()) {
+            for (String uri : uris) {
+                long hits = storage.stream().filter(s -> s.getUri().equals(uri) && !s.getTimestamp().isBefore(start)
+                        && !s.getTimestamp().isAfter(end)
+                        && (unique == null || ips.add(s.getIp()))
+                ).count();
+                result.add(new StatDtoGet(APP, uri, hits));
+            }
+        } else {
+            Set<String> urisSet = new HashSet<>();
+            for (StatModel statModel : storage) {
+                urisSet.add(statModel.getUri());
+            }
+            for (String uri : urisSet) {
+                long hits = storage.stream().filter(s -> s.getUri().equals(uri) && !s.getTimestamp().isBefore(start)
+                        && !s.getTimestamp().isAfter(end) && (unique == null || ips.add(s.getIp()))).count();
+                result.add(new StatDtoGet(APP, uri, hits));
+            }
         }
         return result;
     }
